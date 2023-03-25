5158cdb71c11c88f74a7d3ea5e629d1ba4a89dd9
Cassfsd <perezcasandra@outlook.com>
2023-03-25 08:32:24 -0300

    update

--- jsexample.js
+++ jsexample.js
@@ -13,30 +13,85 @@ El signo de = es de asignacion, le asigna valor a la variable.
 var+ nombre de la variable +signo de asignacion + tipo de dato
 var apellido = "Perez"
 
-Tipos de datos:
+Tipos de datos-:
 -String (cadena): va entre "" o con '' 
 -Number:sirve para neteros y decimales 
 -Boolean o buleanos: son 0 y 1 pero lo traducimos como true y false
--Array o areglo: es una matriz/lista. Los elementos tienen como "posiciones" almacenan multiples valores en una sola variable.
+-Array o areglo: es una matriz/lista. Los elementos tienen "posiciones" almacenan multiples valores en una sola variable. -[]-Tipo de dato objeto. 
+
+-----METODOS PARA OPERAR CON ARRAYS:-----------------------
+En el caso de los arrays, si es un const si lo puedo modificar porque lo que cambia son los datos dentro del array, pero sigue siendo el mismo tipo de dato ,diferente es un string porque ahí cambio el dato directamente. Los arrays tienen indice. Es importante trabajarlos con copia. 
+-Pop:elimina el ultimo elemento del array. nombreDelArray.pop()
+console.log(nombreDelArray)
+-Push:agrega un elemento al final de array. nombreDelArray.push("loQueQuieroAgregar"). 
+Shift:elimina el primer elemento del array
+unshift:agrega un elemento a la primera posicion del array 
+indexof:retorna la posicion del elemento buscado.Devuelve el numero de indice
+includes: devuelve true
+tostring:convierte el array en cadena de caracteres seprados por coma
+join: 
+concat:une los arrays seleciondados en un unico array. 
+
 Este tipo de datos es llamado "primitivos" solo pueden contener una cosa.
 
-Objetos:almacenan colecciones de datos. Se escriben como pares de nombre:valor. Es una variable que tiene propiedades y que esas propiedades tienen un valor */
+Objetos:almacenan colecciones de datos. Se escriben como pares de nombre:valor. Es una variable que tiene propiedades y que esas propiedades tienen un valor. EL objeto va entre llaves y el arreglo entre conchetes*/
 
 var persona = {
     nickName: 'CasandraM',
     apellido: "Perez",
     edad: 31
 };
-/**/ 
+/**/
 
 console.log(persona);
 
 var friends = [/*Variable de tipo array que contiene objetos y dentro valores(strings y numbers)*/
-    { friendName: 'Ana', edad:20 },
-    {friendName:'jose', edad:22},
+    {
+        friendName: 'Ana',
+        edad: 20
+    },
+
+    {
+        friendName: 'jose',
+        edad: 22
+    },
 ];
 
+var datosHijos = [
+    {
+        nombre: "Juan",
+        nacimiento: "17/08/1991",
+        estudia: {
+            colegio:"Rivadavia",
+            direccion:"av corrientes",
+            turno:"mañana"
+        }
+    },
+{
+    nombre:"andres",
+    nacimiento:"18/01/92",
+    estudia:true
+}
+/*Esto (arriba) es un array con 2 objetos*/ 
+];
+console.log(datosHijos);
+console.log(datosHijos[1]); /*Si quiero ver solo el objeto 1 que es andres, porque se empieza a contar desde cero.*/
+console.log(datosHijos[1].nacimiento); /*para ver solo la fecha de nacimiento de andres. Objeto [1] que es andres y .propiedad , que seria nacimiento */
+
 console.log(friends);
+
+var computadora = {
+    procesador: "AMD RADEON",
+    memoriaRAM: "8gb",
+    sistemaOperativo: "Windows11",
+    disco: "solido",
+    tactil: false,
+}
+console.log(computadora.disco); /*Para traer una propiedad de un objeto */
+
+/*Datos complejos-Arrays y objetos*/
+var hijos = ["Juan", "Andres", "Luna"]
+console.log(hijos[2]);/*Para ver solo una posicion especifica */
 
 /*
 Variables: son nombres que le damos a ciertos tipos de datos para almacenarlos en la memoria para luego reutilizarlos.
@@ -129,12 +184,41 @@ switch (typecard) {
 
 /* Clase 8 
 FUNCIONES:*/
-function primeraFuncion(a,b) { 
-  return a+b;
+function primeraFuncion(a, b) {
+    return a + b; 
 
 }
-console.log(primeraFuncion(3,20));
+console.log(primeraFuncion(3, 20));
+// Ejemplo de freecodecamp funciones-----//
+// function sumar (a,b) lo que está entre parentisis son parametros. Pueden tener cualquier nombre. Los valores que asignamos a los parametros se llaman argumentos. 
+
+
+
+
+function sumar (n1,n2){
+    var suma = n1 + n2
+    console.log(suma)
+}
+
+const arrayCosas = ["casa", "auto", "mesa", "piano"]
+
+function recorrerArrayCosas(){
+    for (var i=0;  i<arrayCosas.length; i++){
+        console.log(arrayNombre[i])
+    }
+}
+
+function recorrerArray(array){
+    for (var i=0;  i<array.length; i++){
+        console.log(array[i])
+    }
+}
+
+// Aca termina funciones
 /*-------*/
+
+
+// ----------
 let bienvenida = 'Les damos la bienvenida';
 console.log('bienvenida');
 /*---Revisar esto con fazt youtube----*/
@@ -144,7 +228,7 @@ console.log('bienvenida');
 
 
 
-
+/*LENGHT */
 
 /* Para saber la cantidad de caracteres de un string, uso .lenght: Cuentan los espacios y caracteres especiales*/
 let miNombre = 'Casandra';
@@ -183,7 +267,7 @@ console.log(variable1 && variable3);
 /* Expresiones mas complejas */
 console.log(variable1 && !variable3)
 
-
+/*La variable let no se puede redeclarar en un mismo ambito(scope)*/
 /* Condicionales: If*/
 let nu = 9;
 if (nu < 10) {
@@ -196,10 +280,57 @@ else {
     console.log('nu mayor que 10');
 }
 
+
 /* Bucles: para ejecutar una instrucción o varias instruccciones varias veces
 El bucle for es el más utilizado y lo usamos cuando sabemos la cantidad de veces que vamos a repetir la intruccion */
 
+
+/*BUCLE FOR- La variable se puede llamar diferente a i, pero en programacion se usa mas i, porque es la iteracion, es el iterator.*/
 let nomUsers = ['Hola Bienvenido']
 for (let i = 0; i < 10; i++) { /* Al asignar el ++ incrementamos el valor de la variable en 1 */
     console.log(nomUsers)
+}
+/*MOstrar a cada alumno por consola*/ 
+const listaDeAlumno= ["Ana", "jose","andres"]
+for (let i=0; i< listaDeAlumno.length;i =i + 1){
+    console.log(listaDeAlumno)
+}
+
+// SCOPE-----------------------------------
+// Global:accedidas desde cualquier lugar del progrma 
+// Local:scope de funcion:se pueden accceder desde cualquier lugar dentro de la funcion pero no fuera de ellas y las variables que declaramos con var siempre van a tener un scope de funcion, sin importar donde las declaremos.
+//  y de bloque:aacesibles dentro de todo el bloque pero no fuera de el, un bloque esta dentro de llaves {}, como el de if,while y el de una funcion tambien. Const y let tienen scope de bloque. 
+
+// Las funciones declaradas con var van a estar en memoria durante toda la ejeccuion del programa mientras que las declaradas con const y let solo durante la ejecuccion de la funcion
+
+
+/*Bucle While--no es muy utilizado en la practica ????? */
+
+/*let se puede redeclarar en ambitos distintos y tiene ambitos. Var no tiene ambito
+*/
+
+ let nombre = "pepe"
+if(true){
+    let nombre = "lucas"
+    if(true){
+        console.log(nombre)
+    }
+    if(true){
+        let nombre = "maria"
+        console.log(nombre)
+    }
+    console.log(nombre)
+}
+console.log(nombre) 
+
+/*Con esas notas calcular el promedio de las notas */
+
+// const notas1erSemestre = [3,9,10]
+// let notasAcumuladas = 0
+// for(let i=0; i< notas1erSemestre.length; i=i+1);
+
+// Principio dry:dont repeat yourlsef-----
+
+const saludar = (nombre, apellido) => {
+    return "hola" + nombre  + apellido
 }
